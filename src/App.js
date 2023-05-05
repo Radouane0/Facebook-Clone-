c0b4cb81ef4f79c941683c2f580c9d2a577f46b0
Radouane <ikramaboufera@gmail.com>
2023-05-05 15:15:43 +0200

    Mise en place de la section du profil

--- src/App.js
+++ src/App.js
@@ -4,6 +4,19 @@ import React from "react";
 function App() {
   return (
     <div className="App">
+      <section className="container">
+        <div className="container-left">
+          <h1>facebook</h1>
+          <h2>Connexions récentes</h2>
+          <p>Cliquez sur votre image ou sur Ajouter un compte.</p>
+          <a href=" " className="profile">
+            <i className="fa-solid fa-circle-plus"></i>
+            <h3>Ajouter un compte</h3>
+          </a>
+        </div>
+        <div className="container-right">
+        </div>
+      </section>
       {/* <Footer /> */}
     </div>
   );
